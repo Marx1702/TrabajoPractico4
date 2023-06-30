e4ce477a881543b1137c0cfae74b40732ee261bb
Ignacio Amico <amiconacho1@gmail.com>
2023-06-29 21:57:11 -0300

    cree un input para la clase candidato

--- 01.js
+++ 01.js
@@ -1,11 +1,65 @@
-//DEFINIMOS LA CLASE DE PARTIDO POLITICO
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var readline = require("readline");
+// Función para crear una interfaz de lectura
+function createReadlineInterface() {
+    return readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+}
+// Función para solicitar datos al usuario
+function promptUser(question) {
+    var rl = createReadlineInterface();
+    return new Promise(function (resolve, reject) {
+        rl.question(question, function (answer) {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+// Definición de la clase PartidoPolitico
 var PartidoPolitico = /** @class */ (function () {
     function PartidoPolitico(nombre) {
         this.nombre = nombre;
     }
     return PartidoPolitico;
 }());
-//DEFINIMOS LA CLASE DE CANDIDATO TENIENDO NOMBRE Y A QUE PARTIDO PERTENECEN COMO ATRIBUTOS
+// Definición de la clase Candidato
 var Candidato = /** @class */ (function () {
     function Candidato(nombre, partido) {
         this.nombre = nombre;
@@ -13,18 +67,34 @@ var Candidato = /** @class */ (function () {
     }
     return Candidato;
 }());
-//EN ESTA LISTA INCLUIMOS LOS CANDIDATOS
+// Definición de la clase Lista
 var Lista = /** @class */ (function () {
     function Lista() {
         this.candidatos = [];
     }
-    //FUNCION PARA EL INGRESO AL ARRAY
-    Lista.prototype.agregarCandidato = function (candidato) {
-        this.candidatos.push(candidato);
+    Lista.prototype.agregarCandidato = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var nombre, partido, partidoPolitico, candidato;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, promptUser("Ingrese el nombre del candidato: ")];
+                    case 1:
+                        nombre = _a.sent();
+                        return [4 /*yield*/, promptUser("Ingrese el nombre del partido político: ")];
+                    case 2:
+                        partido = _a.sent();
+                        partidoPolitico = new PartidoPolitico(partido);
+                        candidato = new Candidato(nombre, partidoPolitico);
+                        this.candidatos.push(candidato);
+                        console.log("Candidato agregado correctamente.");
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     return Lista;
 }());
-//DEFINIMOS LA CLASE VOTANTE CON NOMBRE Y DNI
+// Definición de la clase Votante
 var Votante = /** @class */ (function () {
     function Votante(nombre, dni) {
         this.nombre = nombre;
@@ -32,7 +102,7 @@ var Votante = /** @class */ (function () {
     }
     return Votante;
 }());
-//EN LA CLASE VOTO DEFINIMOS COMO ATRIBUTOS QUIEN VOTO Y A QUIEN 
+// Definición de la clase Voto
 var Voto = /** @class */ (function () {
     function Voto(votante, candidato) {
         this.votante = votante;
@@ -40,6 +110,7 @@ var Voto = /** @class */ (function () {
     }
     return Voto;
 }());
+// Definición de la clase EleccionLegislativa
 var EleccionLegislativa = /** @class */ (function () {
     function EleccionLegislativa(distrito, listas) {
         this.distrito = distrito;
@@ -47,6 +118,7 @@ var EleccionLegislativa = /** @class */ (function () {
     }
     return EleccionLegislativa;
 }());
+// Definición de la clase EleccionNacional
 var EleccionNacional = /** @class */ (function () {
     function EleccionNacional(pais, listas) {
         this.pais = pais;
@@ -54,6 +126,7 @@ var EleccionNacional = /** @class */ (function () {
     }
     return EleccionNacional;
 }());
+// Definición de la clase EleccionProvincial
 var EleccionProvincial = /** @class */ (function () {
     function EleccionProvincial(provincia, listas) {
         this.provincia = provincia;
@@ -61,22 +134,23 @@ var EleccionProvincial = /** @class */ (function () {
     }
     return EleccionProvincial;
 }());
-//ACA ASIGNAMOS LOS NOMBRES A LOS PARTIDOS
-var partidoA = new PartidoPolitico('Partido A');
-var partidoB = new PartidoPolitico('Partido B');
-//A LA HORA DE DEFINIR LAS CONSTANTES DE CANDIDATO VA ATRIBUIDO A UN PARTIDO ESPECIFICO
-var candidato1 = new Candidato('Candidato 1', partidoA);
-var candidato2 = new Candidato('Candidato 2', partidoA);
-var candidato3 = new Candidato('Candidato 3', partidoB);
-//DEFINIMOS LA LISTA LEGISLATIVA Y AGREGAMOS LOS CANDIDATOS
-var listaLegislativa = new Lista();
-listaLegislativa.agregarCandidato(candidato1);
-listaLegislativa.agregarCandidato(candidato2);
-//DEFINIMOS LA CONSTANTE LISTA NACIONAL Y AGREGAMOS SUS CANDIDATOS
-var listaNacional = new Lista();
-listaNacional.agregarCandidato(candidato1);
-listaNacional.agregarCandidato(candidato3);
-//CREAMOS NUEVAS INSTANCIAS DE ELECCIONLEGISLATIVA INCORPORANDO LOS DATOS OBTENIDOS
-var eleccionLegislativa = new EleccionLegislativa('Distrito 1', [listaLegislativa]);
-var eleccionNacional = new EleccionNacional('Argentina', [listaNacional]);
-var eleccionProvincial = new EleccionProvincial('Provincia X', [listaLegislativa, listaNacional]);
+// Función principal asincrónica
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var partidoA, partidoB, listaLegislativa;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    partidoA = new PartidoPolitico("Partido A");
+                    partidoB = new PartidoPolitico("Partido B");
+                    listaLegislativa = new Lista();
+                    return [4 /*yield*/, listaLegislativa.agregarCandidato()];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+// Ejecutar la función principal
+main().catch(function (error) { return console.error(error); });
